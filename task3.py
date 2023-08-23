1a1ffe6745c134d8e6662239184d35d9d626851c
IAM-KIRAN <kirankumar.br@terralogic.com>
2023-08-23 11:23:06 +0530

    Updated task3.py for file handling

--- task3.py
+++ task3.py
@@ -6,13 +6,40 @@ while(True):
         response = os.popen(f"ping {ip}").read()
         if "Received = 4" in response:
             print(f"IP address {ip} pinging successfully.")
+            with open("ip.txt", "a") as file:
+                file.write(ip + '\n')
+                print("Email added to the file.")
             print("------------------------------------------")
         else:
             print(f"Pinging IP address {ip} unsuccessful.")
+            with open("invalid_ip.txt", "a") as file:
+                file.write(ip + '\n')
+                print("Email added to the file.")
             print("------------------------------------------")
-    elif input_ip.lower() == "no" or "exit":
+    elif input_ip.lower() == "no" or input_ip.lower() == "exit":
         print("EXITED")
+        print("-------------Valid IPs in file-------------")
+        file_path = "IP.txt"
+        try:
+            with open(file_path, "r") as file:
+                file_content = file.read()
+                print(file_content)
+        except FileNotFoundError:
+            print(f"File '{file_path}' not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
         print("------------------------------------------")
+        file_path2 = "invalid_ip.txt"
+        try:
+            with open(file_path2, "r") as file:
+                file_content2 = file.read()
+                print(file_content2)
+        except FileNotFoundError:
+            print(f"File '{file_path2}' not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        print("------------------------------------------")
+
         break
     else:
         print("Enter valid input.")
