49454a42bfd08ae7eab210cad2706bd015de1b85
IAM-KIRAN <kirankumar.br@terralogic.com>
2023-08-23 14:13:29 +0530

    added regex validation

--- task3.py
+++ task3.py
@@ -1,20 +1,31 @@
 import os
-while(True):
+import re
+
+def valid_ip(ip):
+    pattern = r"(\d{1,3}\.){3}\d{1,3}"
+    return re.match(pattern, ip)
+
+
+while True:
     input_ip = input("Do you want to enter IPs into file: ")
     if input_ip.lower() == "yes":
         ip = input("Enter IP: ")
-        response = os.popen(f"ping {ip}").read()
-        if "Received = 4" in response:
-            print(f"IP address {ip} pinging successfully.")
-            with open("ip.txt", "a") as file:
-                file.write(ip + '\n')
-                print("IP added to the file.")
-            print("------------------------------------------")
+        if valid_ip(ip):
+            response = os.popen(f"ping {ip}").read()
+            if "Received = 4" in response:
+                print(f"IP address {ip} pinging successfully.")
+                with open("ip.txt", "a") as file:
+                    file.write(ip + '\n')
+                    print("IP added to the file.")
+                print("------------------------------------------")
+            else:
+                print(f"Pinging IP address {ip} unsuccessful.")
+                with open("invalid_ip.txt", "a") as file:
+                    file.write(ip + '\n')
+                    print("IP added to the file.")
+                print("------------------------------------------")
         else:
-            print(f"Pinging IP address {ip} unsuccessful.")
-            with open("invalid_ip.txt", "a") as file:
-                file.write(ip + '\n')
-                print("IP added to the file.")
+            print("Please Enter valid input.")
             print("------------------------------------------")
     elif input_ip.lower() == "no" or input_ip.lower() == "exit":
         print("EXITED")
@@ -28,7 +39,7 @@ while(True):
             print(f"File '{file_path}' not found.")
         except Exception as e:
             print(f"An error occurred: {e}")
-        print("------------------------------------------")
+        print("------------Invalid IPs in file------------")
         file_path2 = "invalid_ip.txt"
         try:
             with open(file_path2, "r") as file:
@@ -43,4 +54,7 @@ while(True):
         break
     else:
         print("Enter valid input.")
-        print("------------------------------------------")
+
+
+
+
